0177014501fd75859944ad326f0db187c5b3c63b
Israel Jebaraj C <israeljebaraj.code@gmail.com>
2026-06-15 15:15:56 +0530

    adding swagger for deployment

--- Program.cs
+++ Program.cs
@@ -19,11 +19,8 @@ app.UseCors(options=>{
 });
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
